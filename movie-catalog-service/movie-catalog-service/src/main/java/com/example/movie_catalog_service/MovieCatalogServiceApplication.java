3783479f42771f52a717d224a74833ef236382e9
Md. Rashedul Ghani <rashed.ghani@konasl.com>
2024-10-10 15:15:08 +0600

    Eureka Server Added

--- movie-catalog-service/movie-catalog-service/src/main/java/com/example/movie_catalog_service/MovieCatalogServiceApplication.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/example/movie_catalog_service/MovieCatalogServiceApplication.java
@@ -2,15 +2,17 @@ package com.example.movie_catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
+	@LoadBalanced
 	@Bean
-	public RestTemplate getRestTemplate()
-	{
+	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
 
--- movie-catalog-service/movie-catalog-service/src/main/java/com/example/movie_catalog_service/controller/MovieCatalogController.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/example/movie_catalog_service/controller/MovieCatalogController.java
@@ -2,7 +2,6 @@ package com.example.movie_catalog_service.controller;
 
 import com.example.movie_catalog_service.model.MovieCatalog;
 import com.example.movie_catalog_service.model.MovieInfo;
-import com.example.movie_catalog_service.model.Rating;
 import com.example.movie_catalog_service.model.UserRating;
 import com.example.movie_catalog_service.service.MovieCatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,10 +26,10 @@ public class MovieCatalogController {
     @GetMapping("/{userId}")
     public List<MovieCatalog> getMovieCatalog(@PathVariable("userId") String userId) {
 
-        UserRating userRatings = restTemplate.getForObject("http://localhost:8083/user-rating/"+userId, UserRating.class);
+        UserRating userRatings = restTemplate.getForObject("http://rating-data-service/user-rating/" + userId, UserRating.class);
 
         return userRatings.getRatings().stream().map(rating -> {
-            MovieInfo movieInfo = restTemplate.getForObject("http://localhost:8082/movie-info/" + rating.getMovieId(), MovieInfo.class);
+            MovieInfo movieInfo = restTemplate.getForObject("http://movie-info-service/movie-info/" + rating.getMovieId(), MovieInfo.class);
             return new MovieCatalog(movieInfo.getMovieName(), movieInfo.getDescription(), rating.getRating());
         }).collect(Collectors.toList());
     }
